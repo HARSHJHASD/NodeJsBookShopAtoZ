af43d5140c90f07a6e639d5cdd10cbd8b526b33f
Harsh Jha <harshjha19101997@gmail.com>
2023-11-11 21:52:40 +0530

    Mongoose Adding ,fetching,editing flow completed

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: false },
+  price: { type: Number, required: true },
+  imageUrl: { type: String, required: false },
+});
+
+module.exports = mongoose.model("Product", productSchema);
